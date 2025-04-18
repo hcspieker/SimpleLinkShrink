ab566e08c1fc30ccb8bb1f59d55683592b1fb643
Harald Spieker <14868252+hcspieker@users.noreply.github.com>
2025-04-18 02:42:07 +0200

    setup bootstrap and middleware

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SimpleLinkShrink
-A simple short link web app using ASP.NET Core and SQLite.
+
+A simple short link web app using ASP.NET Core and SQLite. Automatically deployed to [link.spieker.codes](https://link.spieker.codes/) via an Azure DevOps pipeline.
+
+![Build Status](https://harald-spieker.visualstudio.com/Applications/_apis/build/status%2Fhcspieker.SimpleLinkShrink-CI?branchName=main)
--- SimpleLinkShrink/Program.cs
+++ SimpleLinkShrink/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+// not necessary in production due to the use of nginx as a reverse proxy
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseRouting();
+
+app.MapStaticAssets();
+app.MapDefaultControllerRoute().WithStaticAssets();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
